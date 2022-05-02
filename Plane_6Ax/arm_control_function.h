157f0ec7724bd675d7de734e0d229c93a6034185
Vincent-Tann <101033455+Vincent-Tann@users.noreply.github.com>
2022-05-03 01:32:50 +0800

    Update arm_control_function.h

--- Plane_6Ax/arm_control_function.h
+++ Plane_6Ax/arm_control_function.h
@@ -1,2 +1,5 @@
-//包含arm_control_function.cpp文件总函数的声明
+//鍖呭惈arm_control_function.cpp鏂囦欢鎬诲嚱鏁扮殑澹版槑
 #pragma once
+void Connect();
+void Move(double x, double y, double z, double rx, double ry, double rz, double v);
+void GetPosition(double *x, double *y, double *z, double *rx, double *ry, double *rz);
