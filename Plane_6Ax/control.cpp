e25421bc0cb8a9cf972d53f83a2563f1f9613374
Vincent-Tann <101033455+Vincent-Tann@users.noreply.github.com>
2022-05-03 01:34:24 +0800

    Update control.cpp

--- Plane_6Ax/control.cpp
+++ Plane_6Ax/control.cpp
@@ -10,9 +10,13 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include"arm_control_function.h"
+#define pi 3.1416
 
 using namespace std;
 using namespace cv;
+
+double x_init=200, y_init=200, z_init=153;
+//void Move(double x, double y, double z, double rx, double ry, double rz,double v);
 
 /**
  * @name	InitArm
@@ -22,9 +26,21 @@ using namespace cv;
  * @note
  */
 int InitArm() {
-	return 0;
+	//...连接机械臂
+	Connect();
+
+	//移动到初始位置
+	Move(x_init, y_init, z_init, pi / 2, 0, 0,0.01);
+
+	return 1;
 }
 
+
+double dist(Point3d p1, Point3d p2) {
+	double SQR= pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) + pow(p1.z - p2.z, 2);
+	return sqrt(SQR);
+		
+}
 
 /**
  * @name	Glue
@@ -34,7 +50,28 @@ int InitArm() {
  * @return	
  * @note
  */
-void Glue(vector<Point> points,double dt){
+void Glue(vector<Point3d> points,double dt){
+	int num = points.size();//涂胶点的个数
+	Point3d point_init = Point3d(x_init, y_init, z_init);//起始点
+	vector<double> speeds;//速度数组，num+1个
 
-	return ;
+	//计算起始点到第一个涂胶点的速度
+	speeds.push_back(dist(point_init, points[0]) / dt);
+
+	//计算点间的速度
+	for (int i = 0; i < num - 1; i++){//共num个点，求num-1段速度
+		speeds.push_back(dist(points[i], points[i + 1]) / dt);
+	}
+
+	//计算最后一个涂胶点到起始点的速度
+	speeds.push_back(dist(points[num - 1], point_init) / dt);
+
+	//依次移动到num个涂胶点
+	for (int i = 0; i < num; i++)
+	{
+		Move(points[i].x, points[i].y, points[i].z, pi / 2, 0, 0, speeds[i]);
+	}
+	//回到起始点
+	Move(x_init, y_init, z_init, pi / 2, 0, 0, speeds[num]);
+	return;
 }
