fbcd3fd6598d1e68574f5f7b0f66db536f0ce7e4
KarenXD <1350723491@qq.com>
2022-05-06 09:42:08 +0800

    update arm_control

--- Plane_6Ax/arm_control_function.cpp
+++ Plane_6Ax/arm_control_function.cpp
@@ -4,67 +4,11 @@
 ***************************************************************/
 
 /*使用：在Glue/main函数内引用以下线程（？）
-* thread th_socket(pcSocket);
+* thread th_socket(Connect);
 * th_socket.detach();
-* thread th_send(sendMSG, **);
-* th_send.detach();
-* thread th_recv(recvMSG,**);
-* th_recv.detach();
 */
 
-//=============================================================
-#include <stdio.h>
-#include <iostream>
-using namespace std;
-
-/**
- * @name	Connect
- * @brief	连接到机械臂，设置socket通讯
- * @param
- * @return
- * @note
- */
-void Connect() {
-	//...
-	return;
-}
-
-
-/**
- * @name	Move
- * @brief	发送机械臂的位姿
-	输入的x,y,z以mm为单位，rx,ry,rz以rad为单位，速度以mm/s为单位，socket发送前要做单位变换
- * @param	略
- * @return
- * @note
- */
-void Move(double x, double y, double z, double rx, double ry, double rz, double v) {
-	/*
-	测试输出
-	cout << "GLUE POINT" << endl;
-	cout << x << "\t" << y << "\t" << z << endl;
-	*/
-	return;
-}
-
-/**
- * @name	GetPosition
- * @brief	接收机械臂的位姿，并打印
-	接收的x,y,z以mm为单位，rx,ry,rz以rad为单位
- * @param	略
- * @return
- * @note
- */
-void GetPosition(double* x, double* y, double* z, double* rx, double* ry, double* rz) {
-	//socket接收位姿
-	//....
-
-	//打印
-	printf("Receiving current position:%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", *x, *y, *z, *rx, *ry, *rz);
-	return;
-}
-
-//===============================================================================================
+//ps:1.封装ArmPosition? 2.更改urp代码 3。完善getposition
 
 #include "arm_control_function.h"
 
@@ -74,8 +18,14 @@ SOCKET serverSocket;//服务器
 int k = 0;
 
 
-/*以下为服务器（PC）端Socket建立代码*/
-int pcSocket()
+/**
+ * @name	Connect
+ * @brief	连接到机械臂，设置socket通讯
+ * @param
+ * @return
+ * @note
+ */
+void Connect()
 {
 	//加载socket库 版本号
 	WSADATA wsaData;
@@ -141,27 +91,35 @@ int pcSocket()
 		std::cout << "有客户端接入进来！" << inet_ntoa(cAddr.sin_addr) << std::endl;
 		//CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)rcvMSG, (LPVOID)i, NULL, NULL);
 		//CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)sendMSG, (LPVOID)i, NULL, NULL);
-		thread th_send(sendMSG, **);
-		th_send.detach();
-		thread th_recv(rcvMSG, **);
-		th_recv.detach();
+		//thread th_send(Move,x, y, z, rx, ry, rz, v);
+		//th_send.detach();
+		//thread th_recv(GetPosition,LPVOID n);
+		//th_recv.detach();
 	}
-	closesocket(serverSocket);
-	WSACleanup();
+	//closesocket(serverSocket);
+	//WSACleanup();
 
-	return 0;
+	return ;
 }
 
-/*pc端向机械臂发送控制指令，格式为"movej([])\n"*/
-void rcvMSG(LPVOID n)//接受消息
+
+
+/**
+ * @name	GetPosition
+ * @brief	接收机械臂的位姿，并打印
+	接收的x,y,z以mm为单位，rx,ry,rz以rad为单位
+ * @param	略
+ * @return
+ * @note
+ */
+void GetPosition(LPVOID n)//接受消息
 {
 	char buff[256];
 	int r;
 	int i = (int)n;
-	//cout << i << ":" << endl;
+
 	while (1)
 	{
-		//CAR follower;
 		memset(buff, 0, 256);
 		r = recv(clientSocket[i - 1], buff, 255, NULL);
 
@@ -174,24 +132,25 @@ void rcvMSG(LPVOID n)//接受消息
 			int nErrorCode = WSAGetLastError();
 			std::cout << "error=" << nErrorCode << std::endl;
 			exit(0);
-
 		}
-		//memcpy(&follower[i], buff, sizeof(follower[i]));
-		//cout << "x=" << follower[i].x << "\ny="<<follower[i].y<<endl;
 		std::cout << "rcvmsg=" << buff << std::endl;
 	}
 }
-void sendMSG(LPVOID n)//发送消息
+
+
+/**
+ * @name	Move
+ * @brief	发送机械臂的位姿
+	输入的x,y,z以mm为单位，rx,ry,rz以rad为单位，速度以mm/s为单位，socket发送前要做单位变换
+ * @param	略
+ * @return
+ * @note 经调试，速度在10mm/s左右比较合理，太快可能出现问题
+ */
+void Move(double x, double y, double z, double rx, double ry, double rz, double v)//发送消息
 {
-	double x_r, y_r, z_r, rx_r, ry_r, rz_r;//传入的目标坐标（r坐标）\
-	//调试用 
-	/*注：机座坐标系，xyz的单位为m，rx ry rz的单位为rad*/
-	/*在三维形态下不用调整rx ry rz*/
-	x_r = 0.29105; y_r = 0.17851; z_r = 0.42651; rx_r = 2.911; ry_r = -1.2; rz_r = 0;
 	char buff[80];
 	int j;
-	sprintf(buff, "(%lf,%lf,%lf,%lf,%lf,%lf)", x_r, y_r, z_r, rx_r, ry_r, rz_r);
-	Sleep(10);
+	sprintf(buff, "(%lf,%lf,%lf,%lf,%lf,%lf,%lf)", x/1000, y/1000, z/1000, rx, ry, rz, v/1000);
 	for (j = 0; j < k; j++) {
 		int r = send(clientSocket[j], buff, sizeof(buff), NULL);
 	}
